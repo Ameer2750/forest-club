276926023a3047426866c8f6940a5874cf957288
Ameer <ameerbsc2630@gmail.com>
2023-12-05 18:11:49 +0530

    footer content header content navigations added

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,49 @@
-footer
+<div class="flex flex-row justify-even w-full gap-10 px-10 py-6 absolute bottom-0 bg-[#232526] text-white">
+    <div class="w-[25%]">
+        <h5>Contact Us</h5>
+        <br>
+        <p><strong>FOREST CLUB</strong><br>
+            59, Kakaguda Rd, Jyothi Colony,<br> Jupiter Colony, Brooke Bond Colony, <br>Karkhana, Secunderabad, <br>
+            Telangana 500015</p>
+        <div class="w-full h-3.5 border-b border-white "></div>
+
+    </div>
+
+    <div class="w-[25%]">
+        <h5>MAHARASHTRA BRANCH</h5>
+        <br>
+        <p><strong>KB CORPORATION</strong><br>
+            1091, 1st Floor, Above Creative Cloud Anant Balkrushn Naik Road, Shubash Nagar, Shukarwar Peth, Subhash
+            Nagar, Shukrawar Peth, Pune, <br> Maharashtra 411002</p>
+        <div class="w-full h-3.5 border-b border-white "></div>
+
+    </div>
+
+    <div class="w-[25%]">
+        <h5>USEFUL LINKS</h5>
+        <br>
+        <ul class="links">
+
+            <li>Customized T Shirts</li>
+            <li>Casual T Shirts</li>
+            <li>Corporate T Shirts</li>
+            <li>Sports T Shirts</li>
+        </ul>
+    </div>
+
+
+    <div class="w-[25%]">
+        <h5>Location</h5>
+        <div class="w-full">
+            map
+        </div>
+    </div>
+
+
+</div>
+
+
+
+</body>
+
+</html>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,1 +1,91 @@
-Header
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+
+    <!-- icons -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <title>Forest club</title>
+</head>
+
+<body class="w-full h-full">
+    <style>
+        .material-symbols-outlined {
+            font-variation-settings:
+                'FILL' 0,
+                'wght' 400,
+                'GRAD' 0,
+                'opsz' 24
+        }
+    </style>
+
+    <header>
+
+        <div class="flex flex-row justify-between px-14 py-8 ">
+            <div>
+                <img src="{{asset('images/forest-club-logo.png')}}" alt="">
+            </div>
+            <div class="flex flex-row gap-6">
+                <div class="flex flex-row items-center gap-4">
+                    <span class="material-symbols-outlined text-4xl">
+                        phone_iphone
+                    </span>
+                    <span>
+                        Hyderabad
+                        <br>
+                        +91 7999223344
+                    </span>
+                </div>
+                <div class="flex flex-row items-center gap-4">
+                    <span class="material-symbols-outlined text-4xl">
+                        phone_iphone
+                    </span>
+                    <span>
+                        Maharashtra
+                        <br>
+                        +91 9823159663
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <nav class="flex flex-row justify-center bg-black py-4 ">
+                <ul class="flex flex-row  gap-6 text-white">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </header>
