9f63193c8af514353d550f05ffea25d0181f88b1
Ameer <ameerbsc2630@gmail.com>
2023-12-06 12:01:15 +0530

    commit

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,39 +1,76 @@
-<div class="flex flex-row justify-even w-full gap-10 px-10 py-6 absolute bottom-0 bg-[#232526] text-white">
+<style>
+    .material-symbols-outlined {
+        font-variation-settings:
+            'FILL' 0,
+            'wght' 400,
+            'GRAD' 0,
+            'opsz' 24
+    }
+</style>
+
+
+<div class="flex flex-row justify-even w-full gap-10 px-10 py-6  bg-[#232526] text-white">
     <div class="w-[25%]">
-        <h5>Contact Us</h5>
+        <h5 class="text-xl font-semibold">CONTACT US</h5>
         <br>
         <p><strong>FOREST CLUB</strong><br>
             59, Kakaguda Rd, Jyothi Colony,<br> Jupiter Colony, Brooke Bond Colony, <br>Karkhana, Secunderabad, <br>
             Telangana 500015</p>
-        <div class="w-full h-3.5 border-b border-white "></div>
+        <div class="w-full h-2 border-b border-white "></div>
+        <span>
+            092463 75913
+        </span>
 
     </div>
 
     <div class="w-[25%]">
-        <h5>MAHARASHTRA BRANCH</h5>
+        <h5 class="text-xl font-semibold">MAHARASHTRA BRANCH</h5>
         <br>
         <p><strong>KB CORPORATION</strong><br>
             1091, 1st Floor, Above Creative Cloud Anant Balkrushn Naik Road, Shubash Nagar, Shukarwar Peth, Subhash
             Nagar, Shukrawar Peth, Pune, <br> Maharashtra 411002</p>
-        <div class="w-full h-3.5 border-b border-white "></div>
+        <div class="w-full h-2 border-b border-white "></div>
+        <span>09823159663 | 09284052556 | 09975022586
+
+        </span>
 
     </div>
 
     <div class="w-[25%]">
-        <h5>USEFUL LINKS</h5>
+        <h5 class="text-xl font-semibold">USEFUL LINKS</h5>
         <br>
         <ul class="links">
 
-            <li>Customized T Shirts</li>
-            <li>Casual T Shirts</li>
-            <li>Corporate T Shirts</li>
-            <li>Sports T Shirts</li>
+            <li class="flex flex-row items-center">
+                <span class="material-symbols-outlined mt-[0.1rem]">
+                    chevron_right
+                </span>
+                <span>Casual T Shirts</span>
+            </li>
+            <li class="flex flex-row items-center">
+                <span class="material-symbols-outlined mt-[0.1rem]">
+                    chevron_right
+                </span>
+                <span>Customized T Shirts</span>
+            </li>
+            <li class="flex flex-row items-center">
+                <span class="material-symbols-outlined mt-[0.1rem]">
+                    chevron_right
+                </span>
+                <span>Corporate T Shirts</span>
+            </li>
+            <li class="flex flex-row items-center">
+                <span class="material-symbols-outlined mt-[0.1rem]">
+                    chevron_right
+                </span>
+                <span>Sports T Shirts</span>
+            </li>
         </ul>
     </div>
 
 
     <div class="w-[25%]">
-        <h5>Location</h5>
+        <h5 class="text-xl font-semibold">LOCATION</h5>
         <div class="w-full">
             map
         </div>
@@ -42,6 +79,10 @@
 
 </div>
 
+
+<!-- tailwind carosoel -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 
 
 </body>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -12,11 +12,9 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
 
-    <!-- map -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <!-- carousel slider flowbite tailwind -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+
 
     <title>Forest club</title>
 </head>
